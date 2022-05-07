7fa3e9ed3149171c51bbe52313a3cafd4b1f9fb2
Daniela Flores <79023027+DanyFP@users.noreply.github.com>
2022-05-06 18:32:17 -0600

    HTML and CSS part done

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var path = require("path");
+var routes = require("./routes");
+
+var app = express();
+
+//Port
+app.set("port", process.env.PORT || 3000);
+
+//Views
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+
+//Routes
+app.use(routes);
+app.use('/static',express.static('static'));
+
+app.listen(app.get("port"), function () {
+  console.log("Server started on port " + app.get("port"));
+});
