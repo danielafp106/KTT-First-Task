497988333f6f642a0e04a8635268bdf5d042d22f
Daniela Flores <79023027+DanyFP@users.noreply.github.com>
2022-05-08 17:21:38 -0600

    PokeApi part done

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var path = require("path");
 var routes = require("./routes");
-
 var app = express();
+
 
 //Port
 app.set("port", process.env.PORT || 3000);
--- routes.js
+++ routes.js
@@ -1,10 +1,26 @@
 var express = require("express");
 
 var router = express.Router();
+var request = require("request");
+var pokemons;
+
+//Api get
+request(
+  "https://pokeapi.co/api/v2/pokemon?limit=10&offset=90",
+  { json: true },
+  (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    pokemons = body.results;
+  }
+);
 
 router.get("/", function (req, res) {
   console.log("main page");
-  res.render("index");
+  res.render("index",{
+    pokemons: (JSON.stringify(pokemons)),
+  });
 });
 
 module.exports = router;
